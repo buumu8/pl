6d0e14c0a8a7035e69b876f8dca1316331b471ad
Jettapol Tuetrakul <buumu8@gmail.com>
2024-05-06 08:13:51 +0700

    update version

--- src/App.js
+++ src/App.js
@@ -2,35 +2,53 @@ import { useState } from "react";
 
 import "./App.css";
 import data from "./data.json";
-
 function App() {
   const [keyword, setKeyword] = useState("");
   return (
     <div className="App">
-      <div className="search-bar">
+      <div className="header">
         <h1>ค้นหาราคาสินค้า</h1>
-        <h3>อัพเดท 18/10/66</h3>
-        <input
-          type="text"
-          class="searchbox"
-          placeholder="พิมพ์รหัสสินค้า หรือ ชื่อสินค้า"
-          onChange={(event) => {
-            setKeyword(event.target.value.replaceAll(" ", ".*"));
-          }}
-        />
+        <h3>อัพเดท 06/05/67</h3>
+        <div className="searchbar">
+          <button className="clearButton" type="button" onClick={() => setKeyword("")}>
+            ลบ
+          </button>
+          <input
+            type="text"
+            className="searchbox"
+            placeholder="พิมพ์รหัสสินค้า หรือ ชื่อสินค้า"
+            onFocus={() => setKeyword("")}
+            value={keyword}
+            onChange={(event) => {
+              setKeyword(event.target.value.replaceAll(" ", ".*"));
+            }}
+          />
+        </div>
       </div>
       <div className="result">
         {data
-          .filter((d) => keyword.length > 1 && (d.name.search(keyword) >= 0 || d.code === keyword))
+          .filter((d) => {
+            return keyword.length > 2 && (d.name2.toLowerCase().search(keyword.toLowerCase()) >= 0 || d.code === keyword);
+          })
           .map((d, index) => {
             return (
               <div key={`${d.code}-${d.unit}-${d.price}-${index}`} className="result-card">
                 <h3 className="result-card-code">{d.code}</h3>
                 <h2 className="result-card-name">{d.name}</h2>
-                <h2 className="result-card-price">สด รับเอง {parseFloat(d.AA).toLocaleString()}</h2>
-                <h2 className="result-card-price">สด ส่งให้ {parseFloat(d.AB).toLocaleString()}</h2>
-                <h2 className="result-card-price">เชื่อ รับเอง {parseFloat(d.BA).toLocaleString()}</h2>
-                <h2 className="result-card-price">เชื่อ ส่งให้ {parseFloat(d.BB).toLocaleString()}</h2>
+                <h5 className="result-card-name">{d.name2}</h5>
+                <h2 className="result-card-price">
+                  สด รับเอง {parseFloat(d.AA).toLocaleString()} / {parseFloat(d.AA2).toLocaleString()}
+                </h2>
+                <h2 className="result-card-price">
+                  สด ส่งให้ {parseFloat(d.AB).toLocaleString()} / {parseFloat(d.AB2).toLocaleString()}
+                </h2>
+                <h2 className="result-card-price">
+                  เชื่อ รับเอง {parseFloat(d.BA).toLocaleString()} / {parseFloat(d.BA2).toLocaleString()}
+                </h2>
+                <h2 className="result-card-price">
+                  เชื่อ ส่งให้ {parseFloat(d.BB).toLocaleString()} / {parseFloat(d.BB2).toLocaleString()}
+                </h2>
+                {/* <h3>ทุน {parseFloat(d.cost).toLocaleString()}</h3> */}
               </div>
             );
           })}
